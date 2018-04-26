be3cf03e4b463196ffc18e3d2306db6273bc6795
Mehret Kulala <merutaye@gmail.com>
2018-04-25 19:33:45 -0500

    set counter as global variable

--- javascripts/addToArray.js
+++ javascripts/addToArray.js
@@ -2,17 +2,18 @@ const getUser = require('./radioButtonEvent');
 const  dom = require('./dom');
 const data = require('./data');
 const moment = require('moment');
+let counter = 6;
 
 const addToArray = () => {
-  let counter = 6;
   const timestamp = moment().format('MMMM Do YYYY, h:mm:ss');
   console.log(timestamp);
   const newObject = {
     'username': getUser.getUserName(),
     'message': data.saveChanges(),
     'timestamp': timestamp,
-    'id': counter ++,
+    'id': counter,
   };
+  counter = counter ++;
   data.addMessage(newObject);
   dom(data.getMessages());
   console.log(data.message);
