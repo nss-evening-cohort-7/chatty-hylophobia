0e94df6cbfa6d38ce3caf9c4721667ac3645a939
Alex Orga <alexorga.dev@gmail.com>
2018-04-27 16:49:46 -0500

    fixed clear button and merged with master

--- javascripts/addToArray.js
+++ javascripts/addToArray.js
@@ -1,16 +1,16 @@
 const getUser = require('./radioButtonEvent');
 const  dom = require('./dom');
 const data = require('./data');
-// const moment = require('moment');
+const moment = require('moment');
 let counter = 6;
 
 const addToArray = () => {
-  // const timestamp = moment().format('MMMM Do YYYY, h:mm:ss');
-  // console.log(timestamp);
+  const timestamp = moment().format('MMMM Do YYYY, h:mm:ss');
+  console.log(timestamp);
   const newObject = {
     'username': getUser.getUserName(),
     'message': data.saveChanges(),
-    'timestamp': `April 20 2018 4:20pm`, // Replace with timestamp!!!!!!!!!!!
+    'timestamp': timestamp,
     'id': counter,
   };
   counter++;
