d1c6f3c9d20c19d7d938fd80767af8e7a497aecb
Alex Orga <alexorga.dev@gmail.com>
2018-04-27 16:26:26 -0500

    commented out emojis to see what works

--- javascripts/addToArray.js
+++ javascripts/addToArray.js
@@ -1,22 +1,21 @@
 const getUser = require('./radioButtonEvent');
 const  dom = require('./dom');
 const data = require('./data');
-const moment = require('moment');
+// const moment = require('moment');
 let counter = 6;
 
 const addToArray = () => {
-  const timestamp = moment().format('MMMM Do YYYY, h:mm:ss');
-  console.log(timestamp);
+  // const timestamp = moment().format('MMMM Do YYYY, h:mm:ss');
+  // console.log(timestamp);
   const newObject = {
     'username': getUser.getUserName(),
     'message': data.saveChanges(),
-    'timestamp': timestamp,
+    'timestamp': `April 20 2018 4:20pm`, // Replace with timestamp!!!!!!!!!!!
     'id': counter,
   };
   counter++;
   data.addMessage(newObject);
   dom(data.getMessages());
-  console.log(data.message);
 };
 
 module.exports = addToArray;
