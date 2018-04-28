d657d39ac3eca74dc1f1fe81eca0b2255d314b6e
Mehret Kulala <merutaye@gmail.com>
2018-04-28 09:38:19 -0500

    added emoji

--- javascripts/addToArray.js
+++ javascripts/addToArray.js
@@ -2,11 +2,11 @@ const getUser = require('./radioButtonEvent');
 const  dom = require('./dom');
 const data = require('./data');
 const moment = require('moment');
-// let counter = 6;
+let counter = 6;
 
 const addToArray = () => {
   const timestamp = moment().format('MMMM Do YYYY, h:mm:ss');
-  let counter = 6;
+  // let counter = 6;
   console.log(timestamp);
   const newObject = {
     'username': getUser.getUserName(),
