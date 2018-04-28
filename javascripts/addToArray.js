1cae4a0afcb1ca526a6b765d6971a9087d1029bd
Leo Taylor <leotaylor723@gmail.com>
2018-04-28 09:58:58 -0500

    emoji and moment

--- javascripts/addToArray.js
+++ javascripts/addToArray.js
@@ -1,16 +1,16 @@
 const getUser = require('./radioButtonEvent');
 const  dom = require('./dom');
 const data = require('./data');
-// const moment = require('moment');cd
+const moment = require('moment');
 let counter = 6;
 
 const addToArray = () => {
-  // const timestamp = moment().format('MMMM Do YYYY, h:mm:ss');
-  // console.log(timestamp);
+  const timestamp = moment().format('MMMM Do YYYY, h:mm:ss');
+  console.log(timestamp);
   const newObject = {
     'username': getUser.getUserName(),
     'message': data.saveChanges(),
-    // 'timestamp': timestamp,
+    'timestamp': timestamp,
     'id': counter,
   };
   counter++;
