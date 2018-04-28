93ea89bd98459da42773d59063d1592136ed5f64
April Nichols <nicholsar@gmail.com>
2018-04-28 10:37:37 -0500

    edit & delete buttons, message div background

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -3,8 +3,8 @@ const buildDomString = (messagesArray) => {
   let domString = '';
   messagesArray.forEach((message) => {
     domString +=  `<div class="complete-message" id="${message.id}">`;
-    domString +=    `<h5 class="displayed-username">${message.username}:</h5>`;
-    domString +=    `<p class="displayed-time">[${message.timestamp}]:</p>`;
+    domString +=    `<h5 class="displayed-username">${message.username} </h5>`;
+    domString +=    `<p class="displayed-time">[${message.timestamp}]: </p>`;
     domString +=    `<p class="displayed-message">${message.message}</p>`;
     domString +=    `<button class="edit">Edit</button>`;
     domString +=    `<button class="delete-message">Delete</button>`;
