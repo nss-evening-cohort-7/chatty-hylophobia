c24aed809ecb7aa24202b8520614b4715ec53146
April Nichols <nicholsar@gmail.com>
2018-04-28 13:34:51 -0500

    enter listener change

--- javascripts/enterEventListener.js
+++ javascripts/enterEventListener.js
@@ -1,6 +1,5 @@
 const addToArray = require('./addToArray');
 const messageEvents = require('./messageEvents');
-const edit = require('./editEventListener');
 const emojify = require('../lib/node_modules/emojify.js/dist/js/emojify');
 
 const enterListener = () => {
@@ -17,8 +16,6 @@ const keypressEnter = (e) =>
     document.querySelector('#messageInput').value = '';
     emojify.setConfig({'img_dir': './lib/jemoji/emojis',});
     emojify.run(document.getElementById('messages-display'));
-    edit.initEditButton();
-    deleteEventListener.deleteEventListener();
     document.getElementById('messageInput').value = '';
   };
 };
