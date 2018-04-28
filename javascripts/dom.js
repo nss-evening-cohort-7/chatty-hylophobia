fe8692a11cef31b4c31b74071a334b8911b6b2b0
April Nichols <nicholsar@gmail.com>
2018-04-28 12:27:22 -0500

    message display edits

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -3,7 +3,7 @@ const buildDomString = (messagesArray) => {
   let domString = '';
   messagesArray.forEach((message) => {
     domString +=  `<div class="complete-message" id="${message.id}">`;
-    domString +=    `<h5 class="displayed-username">${message.username} </h5>`;
+    domString +=    `<h4 class="displayed-username">${message.username} </h4>`;
     domString +=    `<p class="displayed-time">[${message.timestamp}]: </p>`;
     domString +=    `<p class="displayed-message">${message.message}</p>`;
     domString +=    `<button class="edit">Edit</button>`;
